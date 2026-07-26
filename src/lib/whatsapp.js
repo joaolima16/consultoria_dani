5ab547896acde9824f260f52971b55945dd4c70c
joaolima <joao.goncalves@finnet.com.br>
2026-07-26 11:55:25 -0300

    Update favicon and enhance CSS layout for better responsiveness

--- src/lib/whatsapp.js
+++ src/lib/whatsapp.js
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = '+551191090-1120'
+export const WHATSAPP_NUMBER = '+5511910901120'
 
 export function montarLinkWhatsapp(mensagem) {
   return `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(mensagem)}`
